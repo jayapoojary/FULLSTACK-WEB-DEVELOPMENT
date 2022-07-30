227b28717f960ff9ba66ee05440c2fac44c3e07d
Jaya poojary <jayapoojary862@gmail.com>
2022-07-30 15:05:32 +0530

    Seccond commit

---
+++ Tourism-Management-System-main/travel/common/footer.php
@@ -0,0 +1,63 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							Bangalore, <br> Karnataka, India
+						</div>
+				
+						<div class="footerText">
+							E-mail: jayapoojary862@gmail.com
+						</div>
+						
+					</div>
+					
+					
+					<div class="col-sm-4">
+					</div>
+				
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								facebook.com/tourism_management
+							</div>
+						
+							<div class="gp">
+								www.google.com/tourism_management
+							</div>
+						
+							<div class="tw">
+								twitter.com/tourism_management
+							</div>
+						
+							<div class="in">
+								linkedin.com/tourism_management
+							</div>
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Copyright &copy; 2021 Alisha Anand
+						</div>
+					</div>
+					</div>
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
